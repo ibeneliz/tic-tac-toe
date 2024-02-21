38b4fa8a0089d99703f3af5797cdf34c7bb35cb7
ibeneliz <ibeneliz@gmail.com>
2024-02-21 12:15:18 +0530

    Lifting State Up

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,21 +1,34 @@
+import { useEffect, useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
-    [null, null, null]
+    [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [ ...prevGameBoard.map(innnerArray => [...innnerArray]) ];
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updatedBoard;
+        });
+        onSelectSquare();
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
-            <li key={rowIndex}>
-                <ol>
-                    {row.map((playerSymbol, colIndex) => (
-                    <li key={colIndex}>
-                        <button>{playerSymbol}</button>
-                    </li>))}
-                </ol>
-            </li>))}
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>))}
+                    </ol>
+                </li>))}
         </ol>
     );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
     const [isEditing, setIsEditing] = useState(false);
     const [playerName, setPlayerName] = useState(initialName);
     function handleEditClick() {
@@ -14,7 +14,7 @@ export default function Player({ initialName, symbol }) {
         editablePlayerName = <input type="text" onChange={handleChange} required value={playerName}/>;
     }
     return (
-        <li>
+        <li className={isActive ? "active" : undefined}>
             <span className="player">
                 {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
