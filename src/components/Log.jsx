c2e427a1a39460a4532184e0f64b76b5b20bd172
ibeneliz <ibeneliz@gmail.com>
2024-02-25 00:32:10 +0530

    Lifting computing values up

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,5 +1,11 @@
-const Log = () => {
-    
-};
-
-export default Log;
+export default function Log({ turns }) {
+    return(
+    <ol id="log">
+        {turns.map(turn =>
+            <li key={`${turn.square.row}${turn.square.col}`}>
+                {turn.player} selected {turn.square.row},{turn.square.col}
+            </li>
+        )}
+    </ol>
+    );
+}
